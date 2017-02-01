fe3ff6eb9fcb70f3f6d807d3af4073c1d1394f66
derpom123 <owenmonus@gmail.com>
2017-01-31 20:45:26 -0600

    Added Color

---
+++ ColorV_1.java
@@ -0,0 +1,24 @@
+/**
+ * Created by owen on 2017-01-31.
+ */
+
+
+public class ColorV_1 {
+
+    public static final String ANSI_RESET = "\u001B[0m";
+    public static final String ANSI_BLACK = "\u001B[30m";
+    public static final String ANSI_RED = "\u001B[31m";
+    public static final String ANSI_GREEN = "\u001B[32m";
+    public static final String ANSI_YELLOW = "\u001B[33m";
+    public static final String ANSI_BLUE = "\u001B[34m";
+    public static final String ANSI_PURPLE = "\u001B[35m";
+    public static final String ANSI_CYAN = "\u001B[36m";
+    public static final String ANSI_WHITE = "\u001B[37m";
+
+
+    public static void main(String args[]) {
+        System.out.println(ANSI_BLACK + "T" + ANSI_RESET + ANSI_RED + "h" + ANSI_RESET + ANSI_GREEN + "i" + ANSI_RESET + ANSI_YELLOW + "s" + ANSI_RESET + ANSI_BLUE + " T" + ANSI_RESET + ANSI_PURPLE + "e" + ANSI_RESET + ANSI_CYAN + "x" + ANSI_RESET + ANSI_WHITE + "t" + ANSI_RESET + ANSI_BLACK + " I" + ANSI_RESET + ANSI_RED + "s" + ANSI_RESET + ANSI_GREEN + " R" + ANSI_RESET + ANSI_YELLOW + "A" + ANSI_RESET + ANSI_BLUE + "I" + ANSI_RESET + ANSI_PURPLE + "N" + ANSI_RESET + ANSI_CYAN + "B" + ANSI_RESET + ANSI_WHITE + "Ø" + ANSI_RESET + ANSI_BLACK + "W");
+
+
+    }
+}
