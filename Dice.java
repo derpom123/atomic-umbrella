5072d1823b2313e4ff9d6cb359da924c48f7d09d
derpom123 <owenmonus@gmail.com>
2017-01-31 20:57:57 -0600

    updates to dice

--- Dice.java
+++ Dice.java
@@ -1,20 +1,16 @@
 import java.util.Random;
 
-public class Dice{
+public class Dice {
 	
-	int sides = 6;
+	private int sides = 6;
 
 
 	public int roll(){
-		return new Random().nextInt(6)+1;
+		return new Random().nextInt(sides)+1;
 	}
-
 
 	public static void main(String[] args){
 		Dice ourDice = new Dice();
-
-				
-	
 		System.out.println("Hello, you rolled a " + ourDice.roll() + ". If you got a 1 or a 6 you are very lucky.");
 	}
 }
